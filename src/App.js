d4190c60809e89be4a91df0a7375ef17d7dc803b
Mercynenoh <mercychepngenoh74@gmail.com>
2022-09-26 15:12:18 +0300

    Enpoints with json server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+ const addVideo = async() => {
+    await fetch("http://localhost:3000/videos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  };
+  addVideo()
+
+const viewvideos = async() => {
+  await fetch("http://localhost:3000/videos", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+};
+viewvideos()
+
+
+const editVideo = async(id) => {
+  await fetch(`http://localhost:3000/videos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+};
+editVideo()
+
+
+const deleteVideo = async(id) => {
+  await fetch(`http://localhost:3000/videos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+};
+deleteVideo()
+};
 
 export default App;
